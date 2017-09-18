8db094ab124fbe02c65df23ae02ed397000ca02c
bharathmadishetti <bharathmadishetti@gmail.com>
2017-09-18 08:27:25 +0530

    Update MyClass.java
    Commiting the file change to test SCM

--- MyClass.java
+++ MyClass.java
@@ -1,6 +1,6 @@
 public class MyClass{
 
 	public static void main(String args[]){
-		System.out.println("Testing a java program with jenkins");
+		System.out.println("Testing a java program with jenkins. Modified");
 	}
 }
